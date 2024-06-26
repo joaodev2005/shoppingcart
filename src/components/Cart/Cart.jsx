0977d7583d4ccc109b2914cccefee403eed17201
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-06-26 20:22:45 -0300

    up

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 
 import AppContext from "../../context/AppContext"
 import CartItem from "../CartItem/CartItem"
+import formatCurrency from "../../utils/FormatCurrency"
 
 import './Cart.css'
 
@@ -9,17 +10,20 @@ const Cart = () => {
 
   const { cartItems } = useContext(AppContext)
 
-  const cartResume = cartItems.reduce(() => {} )
+  const cartResume = cartItems.reduce((acumulator, item) => {
+
+    return acumulator + item.price
+  }, 0 )
 
   return (
-    <section className='cart'>
+    <section className='cart cart_active'>
       <div className='cart-itens'>
 
         {cartItems.map(item => <CartItem key={item.id} data={item} />)}
 
       </div>
 
-      <div className='cart-resume'>{cartResume}</div>
+      <div className='cart-resume'>{formatCurrency(cartResume)}</div>
     </section>
   )
 }
