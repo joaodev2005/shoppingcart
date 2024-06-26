e0e513542253283090c019c433e6c0b33eba418a
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-06-26 19:30:04 -0300

    feat: add initial CartItem component

---
+++ src/components/CartItem/CartItem.jsx
@@ -0,0 +1,15 @@
+import './CartItem.css'
+
+const CartItem = () => {
+  return (
+    <section className='cart-item'>
+      <img
+        src=""
+        alt="img do produto"
+        className='cart-item-image'
+      />
+    </section>
+  )
+}
+
+export default CartItem
